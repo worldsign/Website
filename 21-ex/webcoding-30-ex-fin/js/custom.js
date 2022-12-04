a0548feb884a2faf34445267fbfe71203bea6813
worldsign <94952422+worldsign@users.noreply.github.com>
2022-12-04 19:37:28 +0900

    Add files via upload

---
+++ 21-ex/webcoding-30-ex-fin/js/custom.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
